d3e183f981b4cb8199d26d7c8e44846d05e83a1e
KaeShaun <kaeshaun517@revature.net>
2022-05-08 22:20:57 -0500

    stuck

--- Pstarter/src/main/java/Models/Reimbursement.java
+++ Pstarter/src/main/java/Models/Reimbursement.java
@@ -1,5 +1,11 @@
 package Models;
 
 public class Reimbursement {
-
+	private int id;
+	private String author;
+	private String resolver;
+	private String description;
+	private Type type;
+	private Status status;
+	private double amount;
 }
