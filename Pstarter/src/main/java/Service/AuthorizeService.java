d363c2417e8051545a1c51479fae61a7e886260d
KaeShaun <kaeshaun517@revature.net>
2022-05-20 21:47:30 -0500

    update947

---
+++ Pstarter/src/main/java/Service/AuthorizeService.java
@@ -0,0 +1,51 @@
+package Service;
+
+import Models.Users_;
+
+public class AuthorizeService {
+	//make new user object
+	public int register(Users_ userToBeRegistered) {
+		
+		//check  if username exists, so if null username available
+		if(userDAO.getByUsername(userToBeRegistered.getUsername()) != null) {
+			
+			//userame already exists
+			throw new NullPointerException("Username already exists. Modify your input.");
+		}
+		//take in user object sent from menu ad sent to userDAO to insert into Database
+		//after entry's made, the id of the new user's returned
+		return userDAO.create(userToBeRegistered);
+	}
+	
+	public Users_ login(String username, String password) {
+		//temp user
+		Users_ user;
+		
+		//use try+catch to catch any exceptions thrown by userDAO
+		try {
+			//get user data from username given
+			user = userDAO.getByUsername(username);
+			
+			//check if user exists
+			if(user != null) {
+				
+				//check if password is correct
+				if(password.equals(user.getPassword())) {
+					System.out.println("Logged In Successfully!");
+					return user;
+				}else if (!password.equals(user.getPassword())) {
+					System.out.println("Wrong Password. Watch for caps!");
+					return null;
+				}
+			}
+			else {
+				System.out.println("If you would like to create a user and sign in, please contact our HR Department at 555-555-5555. Thank you!");
+				return null;
+			}
+		}catch(Exception e) {
+			System.out.println("Login Unsuccessful");
+			e.printStackTrace();//helpful debugging tool
+		}
+		return null;
+	}
+}
